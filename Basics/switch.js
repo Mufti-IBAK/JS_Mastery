85d621395cb7125c37102035a06fc5b81a54a81b
Mufti-IBAK <muftiibnalkhattab@gmail.com>
2025-03-22 11:31:39 +0100

    still working

---
+++ Basics/switch.js
@@ -0,0 +1,16 @@
+let fruit;
+fruit = "Orange";
+
+switch (fruit) {
+	case "Banana":
+		console.log("Banana is good");
+		break;
+	case "Apple":
+		console.log("How you like them aples");
+		break;
+	case "Orange":
+		console.log("I am not a fan of oranges");
+		break;
+	default:
+		break;
+}
