3d15d679168e5a7e3ec89e5704ecad6e7d76eba0
Mufti-IBAK <muftiibnalkhattab@gmail.com>
2025-03-22 11:58:09 +0100

    more practice

---
+++ objects.js
@@ -0,0 +1,16 @@
+// Task: Create an object representing a book and demonstrate your understanding of objects
+// Create a book object with the following properties:// - title (string)
+// - author (string)// - yearPublished (number)
+// - genres (array of strings)
+// - isAvailable (boolean)// - numberOfPages (number)
+// Then, perform the following operations:// 1. Add a new property called "rating" with a value between 1 and 5
+// 2. Create a method called "toggleAvailability" that changes the isAvailable property// 3. Create a method called "addGenre" that adds a new genre to the genres array
+// 4. Print out all the properties and their values using a for...in loop// 5. Create an array of three book objects with different properties
+// Your code here:
+const book = {
+    // Add properties and methods here};
+
+// Perform operations and demonstrate understanding here
+
+
+}
